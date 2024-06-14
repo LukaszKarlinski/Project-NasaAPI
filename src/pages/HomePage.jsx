b5ccdba5762f9d2f3f75929546e045be1132cb9b
Lukasz Karlinski <mysticsk733@gmail.com>
2024-06-14 14:43:47 +0200

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import '../styles/homePage.scss'
+
+function HomePage(){
+
+    return(
+        <div className="wrapAll">
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+export default HomePage
