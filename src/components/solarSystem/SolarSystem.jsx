f4fa0f0caeba15b187cc5948589dbc2379e8933c
Lukasz Karlinski <mysticsk733@gmail.com>
2024-06-20 23:28:23 +0200

    add planet names

--- src/components/solarSystem/SolarSystem.jsx
+++ src/components/solarSystem/SolarSystem.jsx
@@ -2,6 +2,7 @@ import { Canvas, useLoader } from '@react-three/fiber';
 import { TextureLoader } from 'three';
 import { useState } from 'react';
 import Sphere from './threeJsComponents/Shpere';
+import { Text } from '@react-three/drei'
 import '../../styles/solarSystem.scss'
 
 import sun from '../../assets/textures/sun8k.jpg';
@@ -13,6 +14,7 @@ import jupiter from '../../assets/textures/jupiter2k.jpg';
 import saturn from '../../assets/textures/saturn2k.jpg';
 import uranus from '../../assets/textures/uranus2k.jpg';
 import neptune from '../../assets/textures/neptune2k.jpg';
+import { Group } from 'three/examples/jsm/libs/tween.module.js';
 
 
 
@@ -32,41 +34,49 @@ function SolarSystem(){
 
     const planets = [
         {
+            name: 'mercury',
             positionX: -10,
             delay: 100,
             texture: mercuryTexture,
         },
-        {
+        {   
+            name: 'venus',
             positionX: -7,
             delay: 200,
             texture: venusTexture
         },
-        {
+        {   
+            name: 'earth',
             positionX: -4,
             delay: 300,
             texture: earthTexture 
         },
         {
+            name: 'mars',
             positionX: -1,
             delay: 400,
             texture: marsTexture
         },
         {
+            name: 'jupiter',
             positionX: 2,
             delay: 500,
             texture: jupiterTexture
         },
-        {
+        {   
+            name: 'saturn',
             positionX: 5,
             delay: 600,
             texture: saturnTexture
         },
-        {
+        {   
+            name: 'uranus',
             positionX: 8,
             delay: 700,
             texture: uranusTexture
         },
-        {
+        {   
+            name: 'neptune',
             positionX: 11,
             delay: 800,
             texture: neptuneTexture
@@ -88,7 +98,12 @@ function SolarSystem(){
                 </mesh>
 
                 {planets.map(planet =>{
-                    return <Sphere position={[planet.positionX, y, 0]} size={[1.2,50,50]} texture={planet.texture} delay={planet.delay} handleHidePlanets={handleHidePlanets}/>
+                    return( <group>
+                        <Sphere position={[planet.positionX, y, 0]} size={[1.2,50,50]} texture={planet.texture} delay={planet.delay} handleHidePlanets={handleHidePlanets}/>
+                        <Text position={[planet.positionX, y - 1.5, 0]} fontSize={0.3}>
+                            {planet.name}
+                        </Text>
+                    </group>) 
                 })}
             </Canvas>
         </div>
