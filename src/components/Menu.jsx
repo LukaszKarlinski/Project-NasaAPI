68149a4d1b05cae748dbcd4194afa575f905f22c
Lukasz Karlinski <mysticsk733@gmail.com>
2024-06-22 14:42:23 +0200

    update menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -11,6 +11,7 @@ function Menu(){
         <nav>
             <ul>
                 <li onClick={()=>{handleScroll(1)}}>Picture of the day</li>
+                <li onClick={()=>{handleScroll(2)}}>Our solar system</li>
             </ul>
         </nav>
     )
